d02ac42cd66d91e81fdb6ac757996ded4f15c0a7
vahid <v.yaghini@gmail.com>
2019-03-10 17:36:16 +0100

    clean up

--- src/main/java/com/openclassrooms/watchlist/service/WatchlistService.java
+++ src/main/java/com/openclassrooms/watchlist/service/WatchlistService.java
@@ -8,7 +8,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.openclassrooms.watchlist.controller.WatchlistController;
 import com.openclassrooms.watchlist.domain.WatchlistItem;
 import com.openclassrooms.watchlist.exception.DuplicateTitleException;
 import com.openclassrooms.watchlist.repository.WatchlistRepository;
